f2afc858fc71e567f8d081a5e29173121bc061f8
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-08 17:19:40 +0330

    add method post

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,3 +5,7 @@ from rest_framework.response import Response
 class HomeView(APIView):
     def get(self, request):
         return Response({'message': 'Hello World!'})
+
+    def post(self, request):
+        data = request.data
+        return Response(data)
