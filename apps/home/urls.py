3ecc874b4b6ad56c1513c3a2886c587c00395418
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-08 18:02:53 +0330

    work with serializers

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, PersonView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('person/', PersonView.as_view(), name='person')
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import Person
+from .serializers import PersonSerializer
+
 
 class HomeView(APIView):
     def get(self, request):
@@ -9,3 +12,11 @@ class HomeView(APIView):
     def post(self, request):
         data = request.data
         return Response(data)
+
+
+class PersonView(APIView):
+    def get(self, request):
+        person = Person.objects.all()
+        serializer = PersonSerializer(instance=person, many=True)
+        return Response(serializer.data)
+
