8ac81dd7921d958357b17fc0d9deb4f0f144ca2f
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-12 10:50:10 +0330

    add view post register

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from .serializers import UserRegisterSerializer
+
+
+class UserRegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.POST)
+        if serializer.is_valid():
+            User.objects.create_user(
+                username=serializer.validated_data['username'],
+                email=serializer.validated_data['email'],
+                password=serializer.validated_data['password']
+            )
+            return Response(serializer.data)
+        return Response(serializer.errors)
