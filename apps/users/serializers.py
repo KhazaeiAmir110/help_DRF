b694c1e8308effc963bc0a95c7875e96277c60a7
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-12 15:59:29 +0330

    work with validators

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -3,7 +3,31 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
+def validate_username(attrs):
+    if 'admin' in attrs:
+        raise serializers.ValidationError('admin is not non !!!')
+    return attrs
+
+
+def validate_email(attrs):
+    if '@gmail.com' in attrs:
+        return attrs
+    else:
+        raise serializers.ValidationError('is not a valid email !!!')
+
+
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            },
+            'username': {
+                'validators': [validate_username]
+            },
+            'email': {
+                'validators': [validate_email]
+            }
+        }
