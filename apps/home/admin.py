820a4ce7565c25dfd0622520091215b30d7edf22
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-13 10:27:31 +0330

    create models questions , answer

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Questions, Answer
 
 admin.site.register(Person)
+admin.site.register(Questions)
+admin.site.register(Answer)
