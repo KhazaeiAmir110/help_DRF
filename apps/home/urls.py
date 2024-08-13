83e40d870984e4f0fce5686eb2df23c562ca5f42
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-13 10:40:17 +0330

    add method get in QuestionsView

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, PersonView
+from .views import HomeView, PersonView, QuestionsView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('person/', PersonView.as_view(), name='person')
+    path('person/', PersonView.as_view(), name='person'),
+    path('questions/', QuestionsView.as_view(), name='questions'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,9 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Questions
+from .serializers import PersonSerializer, QuestionsSerializer
 
 
 class HomeView(APIView):
@@ -20,3 +21,18 @@ class PersonView(APIView):
         serializer = PersonSerializer(instance=person, many=True)
         return Response(serializer.data)
 
+
+class QuestionsView(APIView):
+    def get(self, request):
+        questions = Questions.objects.all()
+        serializer = QuestionsSerializer(instance=questions, many=True).data
+        return Response(serializer, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        pass
+
+    def delete(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
