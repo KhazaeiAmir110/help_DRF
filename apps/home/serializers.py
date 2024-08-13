48f34a2f17438821fd204066a9f70da27a9454c8
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-13 10:31:31 +0330

    add serializers

--- apps/home/serializers.py
+++ apps/home/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from apps.home.models import Person
+from apps.home.models import Person, Questions, Answer
 
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
+        fields = '__all__'
+
+
+class QuestionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Questions
+        fields = '__all__'
+
+
+class AnswersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
         fields = '__all__'
