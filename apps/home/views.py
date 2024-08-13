fa321eb82bc1e8de3d5984b54280dbe97dc8be9d
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-08-13 10:44:52 +0330

    add method post in QuestionsView

--- apps/home/views.py
+++ apps/home/views.py
@@ -29,7 +29,11 @@ class QuestionsView(APIView):
         return Response(serializer, status=status.HTTP_200_OK)
 
     def post(self, request):
-        pass
+        serializer = QuestionsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         pass
